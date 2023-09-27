07c3cfdf4a340485fcc5aac5977a94c6b36b5358
Erin Grasmick <burrito@burrito.lan>
2023-09-27 16:32:43 +0100

    not quite

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,84 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use subxt::dynamic::{At, Value, DecodedValueThunk};
+use subxt::dynamic::{At, Value, DecodedValue};
 use subxt::{OnlineClient, PolkadotConfig};
 use subxt::utils;
 use std::str::FromStr;
+
+fn process_locks_data(decoded_locks: &DecodedValue) {
+// Navigate to the main array of locks
+if let Some(locks_array) = decoded_locks.at("value").at("value").at(0) {
+    let mut index = 0;
+
+    // Iterate over each lock in the locks_array
+    while let Some(lock_data) = locks_array.at(index) {
+        // Extract the "id" which is an array of Value items.
+        if let Some(id_comp) = lock_data.at("id") {
+            let mut char_index = 0;
+            let mut id_str = String::new();
+
+            // Iterate over the ID components and assemble the string
+            while let Some(char_val) = id_comp.at(char_index) {
+                if let Some(val) = char_val.as_u128() {
+                    id_str.push(val as u8 as char); // Convert U128 to char
+                }
+                char_index += 1;
+            }
+
+            println!("Lock id: {}", id_str);
+        }
+
+        // Extract the amount from the lock data
+        if let Some(amount_val) = lock_data.at("amount") {
+            if let Some(amount) = amount_val.as_u128() {
+                println!("Amount: {}", amount);
+            }
+        }
+
+        // Extract the reasons from the lock data
+        if let Some(reasons_value) = lock_data.at("reasons") {
+            // If the reasons_value is a variant, its exact structure might be different.
+            // You may need to further process this value based on your requirements.
+            println!("Reasons: {:?}", reasons_value);
+        }
+
+        index += 1; // Move to the next lock in the array
+    }
+} else {
+    println!("Unexpected lock data structure!");
+}
+}
+
+
+async fn fetch_and_print_storage_data(api: &OnlineClient<PolkadotConfig>, module: &str, item: &str, key: Value) -> Result<(), Box<dyn std::error::Error>> {
+    let storage_query = subxt::storage::dynamic(module, item, vec![key]);
+
+    // Fetching the storage data
+    match api.storage().at_latest().await?.fetch(&storage_query).await {
+        Ok(Some(value)) => {
+            match value.to_value() {
+                Ok(decoded_value) => {
+                    // Printing the decoded data
+                    println!("[Decoded Data for {}.{}] {:?}", module, item, decoded_value);
+
+                    // If you want to access specific fields within the decoded data, you can do so:
+                    process_locks_data(&decoded_value);
+                },
+                Err(e) => {
+                    eprintln!("[Error] Converting to value failed for {}.{}: {}", module, item, e);
+                }
+            }
+        },
+        Ok(None) => println!("[{}] Not found for address in {}.{}", item, module, item),
+        Err(e) => {
+            eprintln!("[Error] Fetching failed for {}.{}: {}", module, item, e);
+            return Err(e.into());
+        }
+    };
+
+    Ok(())
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -65,48 +140,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         let key: Value = Value::from_bytes(&public_key_bytes);
-// Fetch general balance
-println!("[Balance] Fetching general balance...");
-let balance_storage_query = subxt::storage::dynamic("Balances", "Account", vec![key.clone()]);
-match api.storage().at_latest().await?.fetch(&balance_storage_query).await {
-    Ok(Some(balance_value)) => {
-        match balance_value.to_value() {
-            Ok(val) => {
-                println!("[Raw Balance Data] {:?}", val); // <-- This line prints the raw data
-                println!("[Balance] Free: {:?}", val.at("data").at("free"));
-            },
-            Err(e) => {
-                eprintln!("[Balance Error] Converting to value failed: {}", e);
-            }
-        }
-    },
-    Ok(None) => println!("[Balance] Not found for address."),
-    Err(e) => {
-        eprintln!("[Balance Error] Fetching failed: {}", e);
+
+    println!("[Balance] Fetching general balance...");
+    if let Err(e) = fetch_and_print_storage_data(&api, "Balances", "Account", key.clone()).await {
+        eprintln!("[Error] Failed to fetch balance: {}", e);
         continue;
     }
-};
 
-        // Fetch locked balance
-        println!("[Locked Balance] Fetching...");
-        let storage_query = subxt::dynamic::storage("Balances", "Locks", vec![key]);
-        match api.storage().at_latest().await?.fetch(&storage_query).await {
-            Ok(Some(value)) => {
-                match value.to_value() {
-                    Ok(val) => {
-                        println!("[Raw Locks Data] {:?}", val); // <-- This line prints the raw data
-                        println!("[Locked Balance] Free: {:?}", val.at("data").at("amount"));
-                    },
-                    Err(e) => eprintln!("[Locked Balance Error] {}", e),
-                }
-            },
-            Ok(None) => println!("[Locked Balance] Not found for address."),
-            Err(e) => {
-                eprintln!("[Locked Balance Error] Fetching failed: {}", e);
-                continue;
-            }
-        };
+    println!("[Locked Balance] Fetching...");
+    if let Err(e) = fetch_and_print_storage_data(&api, "Balances", "Locks", key).await {
+        eprintln!("[Error] Failed to fetch locked balance: {}", e);
+        continue;
     }
+}
 
     println!("\n[Completion] Finished processing all addresses.");
     Ok(())
